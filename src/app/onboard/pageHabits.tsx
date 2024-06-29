142796567363d77cb9583c7609fb512aa9582ac4
Vu <vco.thien@gmail.com>
2024-06-28 21:17:22 -0700

    starting on habits and profile pic

--- src/app/onboard/pageHabits.tsx
+++ src/app/onboard/pageHabits.tsx
@@ -1,13 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import CustomButton from '@/components/CustomButton'
+import { router } from 'expo-router'
 
 const pageHabits = () => {
   return (
-    <View>
-      <Text>pageHabits</Text>
-    </View>
+    <SafeAreaView>
+      <Text>Gaming Habits</Text>
+      <Text>Tell us about how you prefer to play</Text>
+      <View className="absolute bottom-20 left-5">
+        <CustomButton
+          title="Next"
+          handlePress={() => router.push("/onboard/pageDisplay")}
+        />
+      </View>
+    </SafeAreaView>
   )
 }
 
 export default pageHabits
-
--- src/app/onboard/pageProfilePicture.tsx
+++ src/app/onboard/pageProfilePicture.tsx
@@ -1,11 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const pageProfilePicture = () => {
   return (
-    <View>
-      <Text>pageProfilePicture</Text>
-    </View>
+    <SafeAreaView>
+      <Text>Set up your profile</Text>
+      <Text>Tell us about how you prefer to play</Text>
+    </SafeAreaView>
   )
 }
 
