4f27a809c3b5708e26363a1ed73cc06ca287c1f9
Vu <vco.thien@gmail.com>
2024-07-09 19:32:31 -0700

    started on profile pic

--- src/app/onboard/pageProfilePicture.tsx
+++ src/app/onboard/pageProfilePicture.tsx
@@ -1,12 +1,21 @@
 import { Text, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import CustomButton from '@/components/CustomButton'
+import { router } from 'expo-router'
 
 const pageProfilePicture = () => {
   return (
-    <SafeAreaView>
-      <Text>Set up your profile</Text>
-      <Text>Tell us about how you prefer to play</Text>
+    <SafeAreaView className="flex-1 bg-[#101828] p-5">
+      <Text className="text-white text-3xl font-bold">Choose profile picture</Text>
+      <Text className="text-secondary-400 text-xl mt-3 mb-6">Pick a photo that best represents you</Text>
+      <Text>Upload photo</Text>
+      <View className="absolute bottom-20 left-5">
+        <CustomButton
+          title="Next"
+          handlePress={() => router.push("/onboard/pageDisplay")}
+        />
+      </View>
     </SafeAreaView>
   )
 }
